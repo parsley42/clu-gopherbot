950ea9d4a6f9ad9bbecd1fedbd1b77d83219afe3
David Parsley <parsley@linuxjedi.org>
2021-03-05 10:34:24 -0500

    Auto-remove dev containers

--- clu-docker-dev.sh
+++ clu-docker-dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # clu-docker-dev.sh - start clu with gopherbot-dev
-docker run -p 127.0.0.1:3000:3000 --name clu-dev --env-file .env quay.io/lnxjedi/gopherbot-dev:latest
+docker run -p 127.0.0.1:3000:3000 --name clu-dev --rm --env-file .env quay.io/lnxjedi/gopherbot-dev:latest
