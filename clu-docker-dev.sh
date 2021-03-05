743bec17780e855e2f39f64dcd33bc3750510504
David Parsley <parsley@linuxjedi.org>
2021-03-05 09:53:34 -0500

    gopherbot->theia AND theia->gopherbot

--- clu-docker-dev.sh
+++ clu-docker-dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# clu-docker-dev.sh - start the clu dev environment; just tell Clu to "!quit" when done.
-docker run -p 127.0.0.1:3000:3000 --name clu-dev --env-file .env quay.io/lnxjedi/gopherbot-theia:latest
+# clu-docker-dev.sh - start clu with gopherbot-dev
+docker run -p 127.0.0.1:3000:3000 --name clu-dev --env-file .env quay.io/lnxjedi/gopherbot-dev:latest
